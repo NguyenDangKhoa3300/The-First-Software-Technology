86bb21f56c1383633667269724711802bd25c1c7
Trần Minh Khoa <67041926+01675065117@users.noreply.github.com>
2020-11-24 11:56:07 +0700

    GD

---
+++ Giao Diện Chức Năng 789/Chức Năng Gia Hạn Sách/GiaHanSachMain.java
@@ -0,0 +1,26 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GiaHanSachMain extends Application {
+
+	public static void main(String[] args) {
+		launch();
+	}
+
+	@Override
+	public void start(Stage myStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("GiaHanSachController.fxml"));
+		Scene scene = new Scene(root);
+		myStage.setScene(scene);
+		myStage.setTitle("Gia Hạn Sách");
+		myStage.show();
+
+		
+	}
+
+}

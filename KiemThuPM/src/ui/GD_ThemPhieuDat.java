313e112679b1aa6f57cce34c9d5c7dd8beaca9c4
khoavu0310 <72001709+khoavu0310@users.noreply.github.com>
2021-03-20 21:36:40 +0700

    Update GD_ThemPhieuDat.java

--- KiemThuPM/src/ui/GD_ThemPhieuDat.java
+++ KiemThuPM/src/ui/GD_ThemPhieuDat.java
@@ -4,10 +4,12 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
+import dao.DataBase;
 import dao.DataBase;
 import dao.PhieuDatDAO;
 
@@ -22,16 +24,20 @@ import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
 
 public class GD_ThemPhieuDat extends JFrame {
-
+	private final String sql = "SELECT TENNV FROM NHANVIEN";
 	private JPanel contentPane;
-	private JTextField txtTenSach;
-	private JTextField txtMaNSX;
-	private JTextField txtSL;
-	private JTextField txtDonGia;
+	private JComboBox comboBoxTenNV;
+	private PhieuDatDAO phieuDatDAO = new PhieuDatDAO();
+	private String tenNV;
+	private String ngayNhap;
+	private String thangNhap;
+	private String namNhap;
 
 	/**
 	 * Launch the application.
@@ -59,80 +65,101 @@ public class GD_ThemPhieuDat extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblThmPhiut = new JLabel("Thêm Phiếu Đặt");
 		lblThmPhiut.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 26));
 		lblThmPhiut.setBounds(183, 10, 240, 79);
 		contentPane.add(lblThmPhiut);
-		
-		JLabel lblTnSch = new JLabel("Tên sách:");
-		lblTnSch.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblTnSch.setBounds(132, 151, 62, 17);
-		contentPane.add(lblTnSch);
-		
-		JLabel lblMNhSn = new JLabel("Mã nhà sản xuất:");
-		lblMNhSn.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblMNhSn.setBounds(132, 188, 110, 17);
-		contentPane.add(lblMNhSn);
-		
-		JLabel lblSLng = new JLabel("Số lượng:");
-		lblSLng.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblSLng.setBounds(132, 234, 62, 17);
-		contentPane.add(lblSLng);
-		
-		JLabel lblnGi = new JLabel("Đơn giá:");
-		lblnGi.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblnGi.setBounds(132, 267, 54, 17);
-		contentPane.add(lblnGi);
-		//Event nut luu
+		/* Event nut luu*/
 		JButton btnLuu = new JButton("Lưu");
+
 		btnLuu.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String tenSach = txtTenSach.getText();
-				String maNSX = txtMaNSX.getText();
-				String soLuong = txtSL.getText();
-				String donGia = txtDonGia.getText();
-				new PhieuDatDAO().themPhieuDat(tenSach, maNSX, soLuong, donGia);
-				GD_MainPage mainframe = new GD_MainPage().getInstanceOfMainPage();	
+				String timeDat = ngayNhap + "-" + thangNhap + "-" + namNhap;
+				
+			    new PhieuDatDAO().themPhieuDat(tenNV, timeDat);;
+				GD_MainPage mainframe = new GD_MainPage().getInstanceOfMainPage();
 				mainframe.dulieubangPhieuDat();
-				txtTenSach.setText("");
-				txtMaNSX.setText("");
-				txtSL.setText("");
-				txtDonGia.setText("");
-				txtTenSach.requestFocus();
+				
 			}
 		});
+
 		btnLuu.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnLuu.setBounds(240, 368, 110, 29);
 		contentPane.add(btnLuu);
-		
-		txtTenSach = new JTextField();
-		txtTenSach.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		txtTenSach.setBounds(252, 148, 126, 23);
-		contentPane.add(txtTenSach);
-		txtTenSach.setColumns(10);
-		
-		txtMaNSX = new JTextField();
-		txtMaNSX.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		txtMaNSX.setColumns(10);
-		txtMaNSX.setBounds(252, 185, 126, 23);
-		contentPane.add(txtMaNSX);
-		
-		txtSL = new JTextField();
-		txtSL.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		txtSL.setColumns(10);
-		txtSL.setBounds(252, 231, 126, 23);
-		contentPane.add(txtSL);
-		
-		txtDonGia = new JTextField();
-		txtDonGia.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		txtDonGia.setColumns(10);
-		txtDonGia.setBounds(252, 264, 126, 23);
-		contentPane.add(txtDonGia);
-		Toolkit toolkit = Toolkit.getDefaultToolkit();  
-		Dimension screenSize = toolkit.getScreenSize(); 
-		int x = (screenSize.width - getWidth()) / 2;  
-		int y = (screenSize.height - getHeight()) / 2;  
-		setLocation(x, y);  
+
+		JLabel lblChnNhnVin = new JLabel("Chọn Nhân Viên:");
+		lblChnNhnVin.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblChnNhnVin.setBounds(90, 143, 107, 17);
+		contentPane.add(lblChnNhnVin);
+
+		JLabel lblNgy = new JLabel("Ngày đặt:");
+		lblNgy.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNgy.setBounds(90, 194, 62, 17);
+		contentPane.add(lblNgy);
+
+		JLabel lblThngt = new JLabel("Tháng đặt:");
+		lblThngt.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblThngt.setBounds(231, 194, 70, 17);
+		contentPane.add(lblThngt);
+
+		JLabel lblNmt = new JLabel("Năm đặt:");
+		lblNmt.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNmt.setBounds(385, 194, 58, 17);
+		contentPane.add(lblNmt);
+
+		JComboBox comboBoxNgay = new JComboBox();
+		comboBoxNgay.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ngayNhap =  comboBoxNgay.getItemAt(comboBoxNgay.getSelectedIndex()).toString();
+			}
+		});
+		comboBoxNgay.setBounds(162, 194, 50, 21);
+		for (int i = 1; i <= 31; i++) {
+			comboBoxNgay.addItem(i);
+		}
+		contentPane.add(comboBoxNgay);
+
+		JComboBox comboBoxThang = new JComboBox();
+		comboBoxThang.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				thangNhap =  comboBoxThang.getItemAt(comboBoxThang.getSelectedIndex()).toString();
+			}
+		});
+		comboBoxThang.setBounds(323, 194, 50, 21);
+		contentPane.add(comboBoxThang);
+		for (int i = 1; i <= 12; i++) {
+			comboBoxThang.addItem(i);
+		}
+		JComboBox comboBoxNam = new JComboBox();
+		comboBoxNam.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				namNhap =  comboBoxNam.getItemAt(comboBoxNam.getSelectedIndex()).toString();
+			}
+		});
+		comboBoxNam.setBounds(489, 194, 70, 21);
+		contentPane.add(comboBoxNam);
+
+		comboBoxTenNV = new JComboBox();
+		comboBoxTenNV.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tenNV =  (String) comboBoxTenNV.getItemAt(comboBoxTenNV.getSelectedIndex());
+
+			}
+		});
+		comboBoxTenNV.setBounds(215, 143, 135, 21);
+		contentPane.add(comboBoxTenNV);
+		ArrayList<String> dsnv = phieuDatDAO.JComBoBoxNV();
+		for (int i = 0; i < dsnv.size(); i++) {
+			comboBoxTenNV.addItem(dsnv.get(i));
+		}
+		for (int i = 1900; i <= 2021; i++) {
+			comboBoxNam.addItem(i);
+		}
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = toolkit.getScreenSize();
+		int x = (screenSize.width - getWidth()) / 2;
+		int y = (screenSize.height - getHeight()) / 2;
+		setLocation(x, y);
 	}
 }

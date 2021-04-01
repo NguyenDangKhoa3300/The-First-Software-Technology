7d528523c564795f64f84361474597ee7a1e93d2
khoavu0310 <72001709+khoavu0310@users.noreply.github.com>
2021-04-01 20:53:50 +0700

    Update DocGiaDAO.java

--- KiemThuPM/src/dao/DocGiaDAO.java
+++ KiemThuPM/src/dao/DocGiaDAO.java
@@ -6,22 +6,25 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 import entities.DocGia;
 
 public class DocGiaDAO {
-	//ArrayList<DocGia> dsMHP =  new ArrayList<DocGia>();
+	// ArrayList<DocGia> dsMHP = new ArrayList<DocGia>();
 	public DocGiaDAO() {
 
 	}
+
 	public ArrayList<DocGia> doctubang() {
 		Connection con = DataBase.getInstance().getConnection();
 		ArrayList<DocGia> dsDG = new ArrayList<DocGia>();
 		String sql = "select * from DocGia";
 		try {
 			PreparedStatement ps = con.prepareStatement(sql);
-			//ps.setString(1, ma);
+			// ps.setString(1, ma);
 			ResultSet rs = ps.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				String maDG = rs.getString(1);
 				String tenDG = rs.getString(2);
 				String namSinh = rs.getString(3);
@@ -30,7 +33,6 @@ public class DocGiaDAO {
 				DocGia dg = new DocGia(maDG, tenDG, namSinh, cmnd, sdt);
 				dsDG.add(dg);
 			}
-
 
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -46,7 +48,7 @@ public class DocGiaDAO {
 			PreparedStatement ps = con.prepareStatement(sql);
 			ps.setString(1, ma);
 			ResultSet rs = ps.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				String maDG = rs.getString(1);
 				String tenDG = rs.getString(2);
 				String namSinh = rs.getString(3);
@@ -55,11 +57,84 @@ public class DocGiaDAO {
 				DocGia dg = new DocGia(maDG, tenDG, namSinh, cmnd, sdt);
 				list.add(dg);
 			}
-			
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return list;
+	}
+
+	public void themDG(String hoTen, String namSinh, String soCMND, String soDT) {
+
+		String maDG = getLastMaDG();
+
+		try {
+			Connection con = DataBase.getInstance().getConnection();
+			String querry = "Insert into DocGia values('" + maDG + "','" + hoTen + "','" + namSinh + "'," + soCMND + ","
+					+ soDT + ");";
+
+			PreparedStatement ps = con.prepareStatement(querry);
+
+			ps.executeUpdate();
+
+			JOptionPane.showMessageDialog(null, "Added");
+
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public String getLastMaDG() {
+		String tienTo = "maDG_";
+		String toanMa = "";
+		String maDG = "";
+		int max = 1;
+		int hauTo;
+		try {
+			Connection con = DataBase.getInstance().getConnection();
+			String querry = "Select maSV from DocGia";
+
+			PreparedStatement ps = con.prepareStatement(querry);
+
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				toanMa = rs.getString(1);
+				String[] part = toanMa.split("_");
+				hauTo = Integer.parseInt(part[1].trim());
+				if (max < hauTo) {
+					max = hauTo;
+				}
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		max++;
+
+		maDG = tienTo + max;
+
+		return maDG;
+	}
+
+	public void xoaDG(String maDG) {
+
+		try {
+			Connection con = DataBase.getInstance().getConnection();
+			String querry = "delete from DocGia where maSV = '" + maDG + "'";
+
+			PreparedStatement ps = con.prepareStatement(querry);
+
+			ps.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }

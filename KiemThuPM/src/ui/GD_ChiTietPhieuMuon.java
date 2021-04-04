61ce197959d2270f33910aeffbc2a8faf882c0d8
khoavu0310 <72001709+khoavu0310@users.noreply.github.com>
2021-04-04 08:45:58 +0700

    Update GD_ChiTietPhieuMuon.java

--- KiemThuPM/src/ui/GD_ChiTietPhieuMuon.java
+++ KiemThuPM/src/ui/GD_ChiTietPhieuMuon.java
@@ -16,6 +16,8 @@ import dao.CTPhieuThanhLyDAO;
 import entities.SachHienCo;
 import dao.PhieuMuonDAO;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import java.awt.Toolkit;
 import java.util.ArrayList;
@@ -34,6 +36,7 @@ public class GD_ChiTietPhieuMuon extends JFrame {
 	private String tenDG;
 	private CTPhieuThanhLyDAO dsshc = new CTPhieuThanhLyDAO();
 	private PhieuMuonDAO pmd = new PhieuMuonDAO();
+
 	/**
 	 * Launch the application.
 	 */
@@ -49,41 +52,46 @@ public class GD_ChiTietPhieuMuon extends JFrame {
 			}
 		});
 	}
+
 	public void bangdulieuSachHienCo() {
-		DefaultTableModel Df = (DefaultTableModel)tableSachHC.getModel();
-		
+		DefaultTableModel Df = (DefaultTableModel) tableSachHC.getModel();
+
 		ArrayList<SachHienCo> list = dsshc.doctubangSachHienCo();
-		if(list.size()>0) {
-			Df.setRowCount(0);					
-			for (SachHienCo shc : list) {				
-				String[] rowtable = {shc.getMaSach(),shc.getTenSach()};
-				Df.addRow(rowtable);					
-			}					
+		if (list.size() > 0) {
+			Df.setRowCount(0);
+			for (SachHienCo shc : list) {
+				String[] rowtable = { shc.getMaSach(), shc.getTenSach() };
+				Df.addRow(rowtable);
+			}
 			tableSachHC.setModel(Df);
 		}
-		
+
 	}
+
 	public void bangdulieuSachDangMuon() {
-		DefaultTableModel Df = (DefaultTableModel)tableSachMuon.getModel();
-		
+		DefaultTableModel Df = (DefaultTableModel) tableSachMuon.getModel();
+
 		ArrayList<SachHienCo> list = pmd.doctubangSachDangMuon(getMaPM());
-		if(list.size()>0) {
-			Df.setRowCount(0);					
-			for (SachHienCo shc : list) {				
-				String[] rowtable = {shc.getMaSach(),shc.getTenSach()};
-				Df.addRow(rowtable);					
-			}					
+		if (list.size() > 0) {
+			Df.setRowCount(0);
+			for (SachHienCo shc : list) {
+				String[] rowtable = { shc.getMaSach(), shc.getTenSach() };
+				Df.addRow(rowtable);
+			}
 			tableSachMuon.setModel(Df);
 		}
-		
+
 	}
-	public String getMaPM(){
+
+	public String getMaPM() {
 		return this.maPM;
 	}
+
 	public GD_ChiTietPhieuMuon() {
 		initialize();
 	}
-	public GD_ChiTietPhieuMuon(String maPM,String tenDG) {
+
+	public GD_ChiTietPhieuMuon(String maPM, String tenDG) {
 		this.maPM = maPM;
 		this.tenDG = tenDG;
 		initialize();
@@ -91,6 +99,7 @@ public class GD_ChiTietPhieuMuon extends JFrame {
 		bangdulieuSachHienCo();
 		bangdulieuSachDangMuon();
 	}
+
 	/**
 	 * Create the frame.
 	 */
@@ -101,91 +110,83 @@ public class GD_ChiTietPhieuMuon extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 183, 286, 371);
 		contentPane.add(scrollPane);
-		
+
 		tableSachHC = new JTable();
-		tableSachHC.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"M\u00E3 S\u00E1ch ", "T\u00EAn S\u00E1ch"
-			}
-		) {
-			Class[] columnTypes = new Class[] {
-				String.class, String.class
-			};
-			public Class getColumnClass(int columnIndex) {
-				return columnTypes[columnIndex];
-			}
-			boolean[] columnEditables = new boolean[] {
-				false, false
-			};
-			public boolean isCellEditable(int row, int column) {
-				return columnEditables[column];
-			}
-		});
+		tableSachHC.setModel(
+				new DefaultTableModel(new Object[][] {}, new String[] { "M\u00E3 S\u00E1ch ", "T\u00EAn S\u00E1ch" }) {
+					Class[] columnTypes = new Class[] { String.class, String.class };
+
+					public Class getColumnClass(int columnIndex) {
+						return columnTypes[columnIndex];
+					}
+
+					boolean[] columnEditables = new boolean[] { false, false };
+
+					public boolean isCellEditable(int row, int column) {
+						return columnEditables[column];
+					}
+				});
 		tableSachHC.getColumnModel().getColumn(0).setResizable(true);
 		tableSachHC.getColumnModel().getColumn(0).setPreferredWidth(15);
 		tableSachHC.getColumnModel().getColumn(1).setResizable(true);
 		scrollPane.setViewportView(tableSachHC);
-		
+
 		JScrollPane scrollPane_1 = new JScrollPane();
 		scrollPane_1.setBounds(506, 183, 286, 371);
 		contentPane.add(scrollPane_1);
-		
+
 		tableSachMuon = new JTable();
-		tableSachMuon.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"M\u00E3 S\u00E1ch", "T\u00EAn S\u00E1ch"
-			}
-		) {
-			Class[] columnTypes = new Class[] {
-				String.class, String.class
-			};
-			public Class getColumnClass(int columnIndex) {
-				return columnTypes[columnIndex];
-			}
-			boolean[] columnEditables = new boolean[] {
-				false, false
-			};
-			public boolean isCellEditable(int row, int column) {
-				return columnEditables[column];
-			}
-		});
+		tableSachMuon.setModel(
+				new DefaultTableModel(new Object[][] {}, new String[] { "M\u00E3 S\u00E1ch", "T\u00EAn S\u00E1ch" }) {
+					Class[] columnTypes = new Class[] { String.class, String.class };
+
+					public Class getColumnClass(int columnIndex) {
+						return columnTypes[columnIndex];
+					}
+
+					boolean[] columnEditables = new boolean[] { false, false };
+
+					public boolean isCellEditable(int row, int column) {
+						return columnEditables[column];
+					}
+				});
 		tableSachMuon.getColumnModel().getColumn(0).setResizable(true);
 		tableSachMuon.getColumnModel().getColumn(0).setPreferredWidth(15);
 		tableSachMuon.getColumnModel().getColumn(1).setResizable(true);
 		scrollPane_1.setViewportView(tableSachMuon);
-		
+
 		JLabel lblNewLabel = new JLabel("Bảng Sách Đang Có:");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNewLabel.setBounds(71, 130, 131, 17);
 		contentPane.add(lblNewLabel);
-		
+
 		JLabel lblBngSchc = new JLabel("Bảng Sách Độc Đang Giả Mượn:");
 		lblBngSchc.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblBngSchc.setBounds(552, 130, 205, 17);
 		contentPane.add(lblBngSchc);
-		
+
 		JButton btnThm = new JButton("Thêm");
 		btnThm.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				DefaultTableModel Df = (DefaultTableModel) tableSachHC.getModel();
 				int selectedIndex = tableSachHC.getSelectedRow();
 				String maSach = Df.getValueAt(selectedIndex, 0).toString();
-				pmd.themPhieuChiTietMuon(getMaPM(), maSach);
-				bangdulieuSachDangMuon();
+				if (pmd.validationTonTaiSach(maSach)) {
+					pmd.themPhieuChiTietMuon(getMaPM(), maSach);
+					bangdulieuSachDangMuon();
+				} else {
+					JOptionPane.showMessageDialog(null, "Sach da duoc muon!");
+				}
 			}
 		});
 		btnThm.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnThm.setBounds(354, 285, 85, 36);
 		contentPane.add(btnThm);
-		
+
 		JButton btnXoa = new JButton("Xóa");
 		btnXoa.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -199,20 +200,20 @@ public class GD_ChiTietPhieuMuon extends JFrame {
 		btnXoa.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnXoa.setBounds(354, 382, 85, 36);
 		contentPane.add(btnXoa);
-		
+
 		JLabel lblChiTitPhiu = new JLabel("Chi Tiết Phiếu Mượn Của Độc Giả:");
 		lblChiTitPhiu.setFont(new Font("Tahoma", Font.BOLD, 16));
 		lblChiTitPhiu.setBounds(273, 27, 286, 20);
 		contentPane.add(lblChiTitPhiu);
-		
-		lblTenDG = new JLabel("New label",SwingConstants.CENTER);
+
+		lblTenDG = new JLabel("New label", SwingConstants.CENTER);
 		lblTenDG.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblTenDG.setBounds(273, 70, 272, 21);
 		contentPane.add(lblTenDG);
-		Toolkit toolkit = Toolkit.getDefaultToolkit();  
-		Dimension screenSize = toolkit.getScreenSize(); 
-		int x = (screenSize.width - getWidth()) / 2;  
-		int y = (screenSize.height - getHeight()) / 2;  
-		setLocation(x, y);  
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = toolkit.getScreenSize();
+		int x = (screenSize.width - getWidth()) / 2;
+		int y = (screenSize.height - getHeight()) / 2;
+		setLocation(x, y);
 	}
 }
